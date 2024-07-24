7a7520f0f0d76b7bf05f76a8def3cbc4675976ce
Yusuf Mor <yusufmor58@gmail.com>
2024-07-24 17:34:22 +0300

    Bug fixed

--- app/src/main/java/com/example/javacalculator/NumberOnClickListener.java
+++ app/src/main/java/com/example/javacalculator/NumberOnClickListener.java
@@ -3,9 +3,15 @@ package com.example.javacalculator;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NumberOnClickListener implements View.OnClickListener {
     private final int number;
-    private EditText editText;
+    private final EditText editText;
+    private final List<String> semboles = Arrays.asList("+" , "-" ,"%" , "÷" ,"(" , ")" , "×");
 
     public NumberOnClickListener(int number_ , EditText editText_) {
         number = number_;
@@ -14,13 +20,42 @@ public class NumberOnClickListener implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        String mathText = editText.getText().toString();
 
-        if (mathText.endsWith("0")) {
-            mathText = mathText.substring(0 , mathText.length() - 2) + number;
-        } else {
-            mathText += number;
+        if (editText.getText().toString() == null) {
+            editText.setText("");
         }
-        editText.setText(mathText);
+
+        Pattern pattern = Pattern.compile("([-+%÷×\\(\\)]0||^0$)$" ,  Pattern.UNICODE_CASE);
+        String mathText = editText.getText().toString();
+        Matcher m = pattern.matcher(mathText);
+
+
+
+        System.out.println(mathText);
+        boolean value  = m.find();
+        System.out.println(value);
+        String a = "";
+
+
+        if (m.find() && mathText.length() > 0) {
+            String format = mathText.substring(0, mathText.length() - 1) + Integer.toString(number);
+            a = "if";
+            editText.setText(
+                    format
+            );
+            return;
+        } else if (m.find()) {
+            a = "elsif";
+            editText.setText(
+                    Integer.toString(number)
+            );
+            return;
+        } else {
+            a = "else";
+            editText.setText(
+                    mathText + Integer.toString(number)
+            );
+        }
+        System.out.println(a);
     }
 }
